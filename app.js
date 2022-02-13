9d6724ecce66d1d94be46c72dafc2d1f55efe6d9
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-02-13 13:31:44 +0000

    experiments

--- app.js
+++ app.js
@@ -2,4 +2,24 @@
 
 import { simplyWait } from './promises/simplewaiter.mjs';
 
-simplyWait();
+    let initialPromise = new Promise(failExecutor);
+    let waiterPromise = simplyWait( initialPromise, "initial" );
+    let resultPromise = simplyWait( waiterPromise, "waiter" ).catch(
+              e => console.log("ended with catch ", e)
+    );
+    
+    
+
+
+
+function successExecutor( resolve, reject ){
+    resolve("success");
+}
+
+function failExecutor( resolve, reject ){
+    reject("will not");
+}
+
+function throwExecutor( resolve, reject ){
+    throw "cannot";
+}
--- promises/simplewaiter.mjs
+++ promises/simplewaiter.mjs
@@ -1,8 +1,25 @@
 
 
-function simplyWait() {
+function simplyWait(promise, context) {
 
-    console.log("waited");
+    return promise.then( resolved, rejected).catch(thrown );
+
+
+
+function resolved(data) {
+    console.log(context, " resolved ", data);
+    return "good data";
+}
+
+function rejected(reason) {
+    console.log(context, " rejected ", reason);
+    throw "reject " + reason;
+}
+
+function thrown(exception){
+    console.log(context, " exception ", exception);
+    //throw exception;
+}
 
 }
 
