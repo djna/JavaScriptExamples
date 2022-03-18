9b88a03a6fc81906988f0066520d6beba6ab0c9c
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-03-18 10:52:29 +0000

    added awat-to example

--- await.js
+++ await.js
@@ -1,11 +1,77 @@
 
+import to from 'await-to-js';
+// convenience 
+let myto = to.default;
+
 
 async function what() {
 let promiseOne = Promise.resolve( { name: "able", value: 98});
 
 let result = await promiseOne;
 
-console.log("result ", result);
+console.log("Result ", JSON.stringify(result) );
 }
 
-what();
+//what();
+
+async function doWork() {
+    let err, a, b;
+
+    [err, a] = await myto (able(1) );
+
+    if ( a ) {
+       [err, b] = await myto(baker(a.value));
+    }
+
+    if (err){
+        console.log("FFDC error: ", err)
+        // could attempt some alternative processing here
+        // or simple propogate the error
+        return err;
+    } 
+  
+    console.log("recovery processing here")
+    
+    return [err, b];
+}
+
+try {
+   let [err, workResult] = await doWork();
+   if ( err ) {
+       console.log("Worker failed ", err) ;
+   } else {
+       console.log("Work result ", workResult) ;
+   }
+} catch(e) {
+     console.log("catch after call", e); 
+};
+
+
+
+
+function able( aIn ){
+    if ( aIn <= 0 ){
+        return Promise.reject("able needs positive numbers");
+    } else {
+        return Promise.resolve( { name: "able", value: aIn * 23 });
+    }
+}
+
+function baker( bIn ){
+    let thePromise = new Promise(
+        ( resolve, reject ) => 
+            
+            setTimeout(
+                () => {
+                    if ( bIn > 100){
+                        resolve( { name:"baker", value: bIn % 7 } );
+                    } else {
+                        reject("baker needs values bigger than 100");
+                    }
+                }
+             , 1000      
+       )
+    );
+
+  return thePromise;
+}
