827a71e4361bf835efa3b7e164230af308dc79d6
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-11-25 07:50:57 +0000

    camelize stub

---
+++ conventional.js
@@ -0,0 +1,6 @@
+
+export function camelize(plainString){
+
+    return "notImplemented";
+
+}
---
+++ example.js
@@ -0,0 +1,8 @@
+
+import * as convent from './conventional.js'
+
+let wibble = "wibble wobble";
+let camelWibble = convent.camelize(wibble);
+
+console.log(`in ${wibble} to ${camelWibble}`);
+
