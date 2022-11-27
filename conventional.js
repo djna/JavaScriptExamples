7ed5f91d9395194fc9296041c307ed128ffd52a7
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-11-27 06:33:41 +0000

    implement and additional tests

--- conventional.js
+++ conventional.js
@@ -1,6 +1,14 @@
 
 export function camelize(plainString){
 
-    return "notImplemented";
+    let parts = plainString.split(/[\W_]+/);
+
+    let camelizedString = parts.filter(
+        part => part.length > 1
+    ).map(part => 
+        part.substring(0,1).toUpperCase() + part.substring(1).toLowerCase() 
+        ).join("");
+
+    return camelizedString
 
 }
--- test/conventional.js
+++ test/conventional.js
@@ -12,4 +12,44 @@ describe('camelize', function() {
 
         expect(camelCaseText).to.equal("HumanReadableText");
     });
+
+    it('should manage embedded capitals', function () {
+        let humanText = "Human readable TEXT";
+
+        let camelCaseText = convent.camelize(humanText);
+
+        expect(camelCaseText).to.equal("HumanReadableText");
+    });
+
+    it('should ignore leading spaces', function () {
+        let humanText = "   Human readable TEXT";
+
+        let camelCaseText = convent.camelize(humanText);
+
+        expect(camelCaseText).to.equal("HumanReadableText");
+    });
+
+    it('should remove punctuation before camelize]ing ', function () {
+        let humanText = "Human readable-text_with+punctuation";
+
+        let camelCaseText = convent.camelize(humanText);
+
+        expect(camelCaseText).to.equal("HumanReadableTextWithPunctuation");
+    });
+
+    it('should treat adjacent spaces and punctuation as a single space ', function () {
+        let humanText = "Human   readable---text__with+''punctuation";
+
+        let camelCaseText = convent.camelize(humanText);
+
+        expect(camelCaseText).to.equal("HumanReadableTextWithPunctuation");
+    });
+
+    it('should remove single character words ', function () {
+        let humanText = "Human's text a ";
+
+        let camelCaseText = convent.camelize(humanText);
+
+        expect(camelCaseText).to.equal("HumanText");
+    });
 });
