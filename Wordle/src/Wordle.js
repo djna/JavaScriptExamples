ff2aeb8aee8c8d8c5caeb7852f4328ad1c34f7f3
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-14 14:42:24 +0000

    await for guesses

--- Wordle/src/Wordle.js
+++ Wordle/src/Wordle.js
@@ -10,26 +10,26 @@ class Wordle {
 
     constructor() {
         this.dictionary = ["anvil", "brick", "crisp", "dance"];
+
+        this.rl = readline.createInterface({ input, output });
+
+        this.prompt = (query) => new Promise((resolve) => this.rl.question(query, resolve));
     }
 
-    async playGame() {
-
-        const rl = readline.createInterface({ input, output });
-
-        const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+    async runWordle() {
 
         let exiting = false;
 
         try {
             while (!exiting) {
 
-                let answer = await prompt('(P)lay) or e(X)it? ');
+                let answer = await this.prompt('(P)lay) or e(X)it? ');
                 answer = answer.toUpperCase();
                 if (answer === "X") {
                     console.log(`Exit requested.`);
                     exiting = true;
                 } else if (answer === "P") {
-                    console.log(`Not ready to play`);
+                    await this.playGame();
                 } else {
                     console.log(`I don't know how to "${answer}"`);
                 }
@@ -41,11 +41,35 @@ class Wordle {
 
         console.log(`Thank you for visiting, goodbye.`);
 
-        rl.close();
+        this.rl.close();
 
+    }
+
+    async playGame(){
+        let target = this.dictionary[0];
+        let checker = new WordChecker(target);
+        let done = false;
+        let attempts = 0;
+        while ( (! done) && attempts <= 6 ){
+            attempts++;
+            let guess =  await this.prompt(`${attempts}). Enter guess or :q to stop? `);
+            if ( guess === ":q"){
+                done = true;
+            } else {
+                let result = checker.check(guess);
+                if ( result.solved ){
+                    console.log(`solved :${result.guess}: in ${attempts}`);
+                    done = true; 
+                }else {
+                    console.log(`match:${[...result.indicators].join("")}:`)
+                }
+            }
+        }
+        
     }
 }
 
 
+
 let wordle = new Wordle();
-wordle.playGame();
+wordle.runWordle();
