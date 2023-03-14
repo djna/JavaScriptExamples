e014ff60bbd843bde090cc6197cedd2e8b91b843
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2023-03-14 16:05:58 +0000

    read arbitrary dictionary

--- Wordle/src/Wordle.js
+++ Wordle/src/Wordle.js
@@ -2,6 +2,7 @@ import { WordChecker } from "./WordChecker.js";
 
 import * as readline from 'readline';
 import { stdin as input, stdout as output } from 'node:process';
+import { readFileSync } from 'fs';
 
 import 'babel-polyfill';
 
@@ -9,7 +10,14 @@ import 'babel-polyfill';
 class Wordle {
 
     constructor() {
-        this.dictionary = ["anvil", "brick", "crisp", "dance"];
+        let filePath = "Wonder.txt";
+        const data = readFileSync(filePath, 'utf-8');
+        const regex = /\b\w{5}\b/g;
+        this.dictionary = [...data.matchAll(regex)].map(
+            (entry) => entry[0]
+        );
+
+        console.log(`Dictionary: ${this.dictionary}`);
 
         this.rl = readline.createInterface({ input, output });
 
@@ -46,6 +54,7 @@ class Wordle {
     }
 
     async playGame(){
+     
         let target = this.dictionary[Math.floor(Math.random()*this.dictionary.length)];
         let checker = new WordChecker(target);
         let done = false;
@@ -55,17 +64,20 @@ class Wordle {
             let guess =  await this.prompt(`${attempts}). Enter guess or :q to stop? `);
             if ( guess === ":q"){
                 done = true;
+                console.log(`The answer was :${target}:`);
+                return;
             } else {
                 let result = checker.check(guess);
                 if ( result.solved ){
                     console.log(`solved :${result.guess}: in ${attempts}`);
                     done = true; 
+                    return;
                 }else {
                     console.log(`match:${[...result.indicators].join("")}:`)
                 }
             }
         }
-        if ( attempts >= 6){
+        if ( attempts >= 6 ){
             console.log(`Unlucky, the answer was :${target}:`);
         }
         
